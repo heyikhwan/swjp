6153ba0d870fe5a39d71ab8fbb06530d7ca72e7d
fadilmartias <fadilmartias26@gmail.com>
2022-06-20 13:18:02 +0700

    fix views

--- storage/framework/views/9dbd1310919e42f9678a5d987df90bb5a04d5b92.php
+++ storage/framework/views/9dbd1310919e42f9678a5d987df90bb5a04d5b92.php
@@ -1,12 +1,14 @@
-<?php $__env->startSection('title'); ?> <?php echo app('translator')->get('translation.Data_Tables'); ?>  <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('title'); ?> Data Wilayah <?php $__env->stopSection(); ?>
 <?php $__env->startSection('css'); ?>
 <link href="<?php echo e(URL::asset('/assets/libs/datatables.net-bs4/datatables.net-bs4.min.css')); ?>" rel="stylesheet">
-<link href="<?php echo e(URL::asset('assets/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(URL::asset('assets/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css')); ?>"
+    rel="stylesheet" type="text/css" />
 <link href="<?php echo e(URL::asset('assets/libs/sweetalert2/sweetalert2.min.css')); ?>" rel="stylesheet">
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
 <?php $__env->startComponent('components.breadcrumb'); ?>
-<?php $__env->slot('li_1'); ?> Data Master <?php $__env->endSlot(); ?>
+<?php $__env->slot('li_1'); ?> Home <?php $__env->endSlot(); ?>
 <?php $__env->slot('title'); ?> Data Wilayah <?php $__env->endSlot(); ?>
 <?php echo $__env->renderComponent(); ?>
 
@@ -23,56 +25,83 @@
 <?php endif; ?>
 
 <div class="row">
-    <div class="col-12">
-        <div class="card">
+    <div class="col-lg-12">
+        <div class="card mb-0">
             <div class="card-body">
                 <div class="row align-items-center">
-                    <div class="col-md-12">
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
                         <div class="d-flex flex-wrap align-items-center justify-content-end gap-2 mb-3">
                             <div>
-                                <a href="<?php echo e(route('wilayah.create')); ?>" class="btn btn-light"><i class="bx bx-plus me-1"></i> Tambah Wilayah</a>
+                                <a href="<?php echo e(route('wilayah.create')); ?>" class="btn btn-light"><i
+                                        class="bx bx-plus me-1"></i> Tambah Wilayah</a>
                             </div>
                         </div>
+
                     </div>
                 </div>
                 <!-- end row -->
 
-                <table id="wilayah" class="table align-middle dt-responsive table-check nowrap"
-                style="border-collapse: collapse; border-spacing: 0 8px; width: 100%;">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Wilayah</th>
-                            <th>Level</th>
-                            <th>Induk Wilayah</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                </table>
+                <div class="table-responsive mb-4">
+                    <table class="table align-middle datatable dt-responsive table-check nowrap"
+                        style="border-collapse: collapse; border-spacing: 0 8px; width: 100%;">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Level</th>
+                                <th style="width: 80px; min-width: 80px;">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $__currentLoopData = $wilayah; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr>
+                                <td><?php echo e($loop->index + 1); ?></td>
+                                <td><?php echo e($item->nama); ?></td>
+                                <td>
+                                    <?php if($item->level == 1): ?>
+                                    Provinsi
+                                    <?php elseif($item->level == 2): ?>
+                                    Kabupaten/Kota
+                                    <?php elseif($item->level == 2): ?>
+                                    Kecamatan
+                                    <?php else: ?>
+                                    Desa
+                                    <?php endif; ?>
+                                </td>
+                                <td class="d-flex gap-2 align-items-center">
+                                    <a href="<?php echo e(route('kendaraan.edit', $item->id)); ?>"
+                                        class="btn btn-soft-warning waves-effect waves-light">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+
+                                    <button type="button" class="btn btn-soft-danger waves-effect waves-light"
+                                        id="sa-warning" onclick="swal(<?php echo e($item->id); ?>)">
+                                        <i class="fas fa-trash"></i>
+
+                                        <form action="<?php echo e(route('kendaraan.destroy', $item->id)); ?>" method="post"
+                                            id="delete-<?php echo e($item->id); ?>">
+                                            <?php echo csrf_field(); ?>
+                                            <?php echo method_field('delete'); ?>
+                                        </form>
+                                    </button>
+                                </td>
+                            </tr>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </tbody>
+                    </table>
+                    <!-- end table -->
+                </div>
+                <!-- end table responsive -->
             </div>
         </div>
-    </div> <!-- end col -->
-</div> <!-- end row -->
-
-<!-- Modal -->
-<div class="modal fade" id="rating" tabindex="-1" aria-labelledby="ratingLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="ratingLabel">Beri Rating</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
     </div>
-  </div>
 </div>
-
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('script'); ?>
 <script src="<?php echo e(URL::asset('assets/libs/datatables.net/datatables.net.min.js')); ?>"></script>
@@ -127,5 +156,4 @@
     }
 </script>
 <?php $__env->stopSection(); ?>
-
 <?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\swjp\resources\views/backend/wilayah/index.blade.php ENDPATH**/ ?>
